e2390ba6fd93014a692e3a3fa1f4bd9ea28dc213
ZANYRI <zanyri880@gmail.com>
2026-02-17 17:20:40 +0300

    update request

---
+++ request/gemini.go
@@ -0,0 +1,40 @@
+package request
+
+import (
+	"context"
+	"log"
+
+	"google.golang.org/genai"
+
+	"ai-api/env"
+)
+
+func TestFunc(name_key, message string) string {
+
+	key, _ := env.LoadEnv(name_key) 
+
+    ctx := context.Background()
+
+	cc := genai.ClientConfig{
+		APIKey: key,
+	}
+
+    client, err := genai.NewClient(ctx, &cc)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    result, err := client.Models.GenerateContent(
+        ctx,
+        "gemini-3-flash-preview",
+        genai.Text(message),
+        nil,
+    )
+    if err != nil {
+        log.Fatal(err)
+    }
+
+	res := result.Text()
+
+	return res
+}
